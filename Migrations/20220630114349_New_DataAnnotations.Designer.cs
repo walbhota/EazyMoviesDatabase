0a7f4e24566b45aa96c9cb67526b8c16e2ccbdbf
j8 <walbhota@gmail.com>
2022-06-30 13:21:57 +0100

    Added validation

--- Migrations/20220630114349_New_DataAnnotations.Designer.cs
+++ Migrations/20220630114349_New_DataAnnotations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EazyMoviesDatabase.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EazyMoviesDatabase.Migrations
 {
     [DbContext(typeof(EazyMoviesContext))]
-    partial class EazyMoviesContextModelSnapshot : ModelSnapshot
+    [Migration("20220630114349_New_DataAnnotations")]
+    partial class New_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.6");
